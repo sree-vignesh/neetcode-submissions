ad95e6893dd53873777ffcd8497cf3fbe75f6327
Sree Vignesh <101454034+sree-vignesh@users.noreply.github.com>
2026-06-16 20:58:44 +0530

    Add: longest-common-prefix - submission-42

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-42.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        // System.out.println(isSame(strs,2));
+        int index = 0;
+        for (int i = 0; i < strs[0].length(); i++) {
+            if (isSame(strs, i)) {
+                index++;
+            } else {
+                break;
+            }
+        }
+        return strs[0].substring(0, index);
+    }
+
+    public static boolean isSame(String[] strs, int index) {
+        for (String x : strs) {
+            if (x.length() <= index) {
+                return false;
+            }
+        }
+        // if(strs[0].length() <= index){
+        //     return false;
+        // }
+
+        char chr = strs[0].charAt(index);
+
+        for (String x : strs) {
+            if (x.charAt(index) != chr) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
